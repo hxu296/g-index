309bb405a544c899d7e6bde872ec3526a9ca376e
Huan Xu <hxu296@wisc.edu>
2022-03-21 03:17:46 -0400

    update webpage.png

--- ui.R
+++ ui.R
@@ -3,6 +3,7 @@ library(ggplot2)
 
 ui <- fluidPage(
     h1("University Ranking By Github Contribution"),
+    h5("initial loading may take a few seconds"),
     plotOutput("rankingBarPlot", click = "rankBarClick"),
     fluidRow(
       column(7, plotOutput("repoBarPlot")),
